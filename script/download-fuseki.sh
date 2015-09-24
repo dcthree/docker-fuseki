1862c67dae65d977357152b8eedf70e9ecc6668f
Ryan Baumann <ryan.baumann@gmail.com>
2015-09-24 14:43:02 -0400

    Fix fuseki download, lock distribution to the same one used in development

--- script/download-fuseki.sh
+++ script/download-fuseki.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-distr=`curl -silent https://www.apache.org/dist/jena/binaries/ | perl -ne '/href="(jena-fuseki-\d\.\d\.\d)-distribution.tar.gz"/ && print $1'`
-wget --quiet https://www.apache.org/dist/jena/binaries/$distr-distribution.tar.gz -O- | tar -zx --strip-components 1 -C /opt/fuseki/ $distr
+distr="jena-fuseki-1.1.0"
+wget --no-check-certificate https://archive.apache.org/dist/jena/binaries/$distr-distribution.tar.gz -O-|tar -zx --strip-components 1 -C /opt/fuseki/ $distr
