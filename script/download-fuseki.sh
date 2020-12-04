8b31fc7a9db3817968c47cdb5ef376fcbdbbd626
Ryan Baumann <ryan.baumann@gmail.com>
2020-12-04 10:38:17 -0500

    Update to focal, JDK 11, Fuseki 3.17.0

--- script/download-fuseki.sh
+++ script/download-fuseki.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-distr="jena-fuseki-1.1.0"
-wget --no-check-certificate https://archive.apache.org/dist/jena/binaries/$distr-distribution.tar.gz -O-|tar -zx --strip-components 1 -C /opt/fuseki/ $distr
+distr="apache-jena-fuseki-3.17.0"
+wget --no-check-certificate https://archive.apache.org/dist/jena/binaries/$distr.tar.gz -O-|tar -zx --strip-components 1 -C /opt/fuseki/ $distr
